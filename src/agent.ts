874bc8931bbef94438c8b8366b5834649ffdff79
mch <omitted@email.com>
2024-12-26 01:43:21 -0500

    pass answer to runLLM

--- src/agent.ts
+++ src/agent.ts
@@ -5,25 +5,32 @@ import { ToolT } from './types.ts';
 import { logMessage, showLoader } from './ui.ts';
 
 export async function runAgent({
+  turns = 10,
   userMessage,
   tools = [],
 }: {
+  turns: number;
   userMessage: string;
   tools: ToolT[];
 }) {
   await addMessages([{ role: 'user', content: userMessage }]);
   const loader = showLoader('🤔');
-  const history = await getMessages();
-  const response = await runLLM({ messages: history, tools });
-  await addMessages([response]);
-  if (response.tool_calls) {
-    const toolCall = response.tool_calls[0];
-    loader.update(`executing ${toolCall.function.name}`);
-    const toolResponse = await runTool(toolCall, userMessage);
-    await saveToolResponse(toolCall.id, toolResponse);
-    loader.update(`executed ${toolCall.function.name}`);
+  while (true) {
+    const history = await getMessages();
+    const response = await runLLM({ messages: history, tools });
+    await addMessages([response]);
+    if (response.content) {
+      loader.stop();
+      logMessage(response);
+      return await getMessages();
+    }
+    if (response.tool_calls) {
+      const toolCall = response.tool_calls[0];
+      logMessage(response);
+      loader.update(`executing ${toolCall.function.name}`);
+      const toolResponse = await runTool(toolCall, userMessage);
+      await saveToolResponse(toolCall.id, toolResponse);
+      loader.update(`executed ${toolCall.function.name}`);
+    }
   }
-  loader.stop();
-  logMessage(response);
-  return getMessages();
 }
